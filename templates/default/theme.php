0f34efbce36b00683b5463f7293bde9a1c8682fb
Erik Simmesgård <erik-github@simmesgard.se>
2012-07-10 19:19:58 +0200

    Fix relative location of lock.png

--- templates/default/theme.php
+++ templates/default/theme.php
@@ -174,7 +174,7 @@ if (isset($_GET['archive']))
 	
 	while ($row = mysql_fetch_array($pastes))
 	{
-      $pass = ($row['password'] == "EMPTY") ? "" : "<img src=\"templates/default/images/lock.png\" title=\"Password protected\" />";
+      $pass = ($row['password'] == "EMPTY") ? "" : "<img src=\"" . $CONF['url'] . 'templates/' . $CONF['template'] . "/images/lock.png\" title=\"Password protected\" />";
 		echo "<tr>";
       echo "<td>" . $pass . "</td>";
 		echo "<td class=\"padright\"><a title=\"" . date("l F j, Y, g:i a", strtotime($row['posted'])) . "\" href=\"". $CONF['pastebin'] . "/" . $row['pid'] . "\">" . $row['poster'] . "</a></td>";
