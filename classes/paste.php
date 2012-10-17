d23d5ca706e5c1c775f59a3a88f6f4c107a6b68f
Erik Simmesgård <erik-github@simmesgard.se>
2012-10-17 22:49:05 +0200

    Resetting parent_pid, fixes #8

--- classes/paste.php
+++ classes/paste.php
@@ -289,7 +289,13 @@ class Pastebin
 					$post['parent_postdate']=$parent['postdate'];
 					$post['parent_diffurl']=$this->conf['diff_url']."$pid";
 					
-				}
+                } else {
+                    /* If parent_pid is larger than zero but $parent is null, then
+                       the parent paste has probably been cleaned out.
+                       We could clean up the database, but I'd rather keep this
+                       metadata around - so that we could display it in presentation
+                       layer if we wanted to. */
+                    $post['parent_pid'] = 0;
 			}
 	
 			// Amendments
